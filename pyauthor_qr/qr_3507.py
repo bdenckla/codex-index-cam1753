edbc16e6f0af81379fe4c3cb4c3db24fba03f691
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-01 14:02:27 -0500

    improve wording of 4 records

--- pyauthor_qr/qr_3507.py
+++ pyauthor_qr/qr_3507.py
@@ -3,7 +3,7 @@ RECORD_3507 = {
     "qr-cv": "35:7",
     "qr-consensus": "מַה־מִּיָּדְךָ֥",
     "qr-lc-proposed": "מַה־מִיָּדְךָ֥",
-    "qr-what-is-weird": "lacks דגש in מ (DM footnote 25)",
+    "qr-what-is-weird": "מ lacks דגש (DM footnote 25)",
     "qr-lc-loc": {"page": "407A", "column": 2, "line": 5},
     "qr-under-construction": True,
 }

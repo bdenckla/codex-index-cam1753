679140e90837bc7bef1df8d824ef4551c897fef4
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-03 00:33:24 -0500

    6:17 is tBHQ

--- pyauthor_qr/qr_0617.py
+++ pyauthor_qr/qr_0617.py
@@ -1,9 +1,9 @@
 RECORD_0617 = {
-    "qr-noted-by": "nDM",
+    "qr-noted-by": "tBHQ-nDM",
     "qr-cv": "6:17",
     "qr-consensus": "בְּעֵ֣ת",
     "qr-lc-proposed": "בְּ֭עֵת",
     "qr-what-is-weird": "דחי not מונח",
-    "qr-lc-loc": {"page": "398B", "column": 2, "line": 16},
+    "qr-lc-loc": {"page": "398B", "column": 2, "line": 15},
     "qr-under-construction": True,
 }

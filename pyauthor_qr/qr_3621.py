c86c4abe4a009336658ef213d99b89b37cdd8897
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-04 14:41:20 -0500

    rm stuff about Venice MG

--- pyauthor_qr/qr_3621.py
+++ pyauthor_qr/qr_3621.py
@@ -1,26 +1,3 @@
-# The Venice MG (RB2) has מרכא; see DM footnote 16.
-# _DM_FTNT_16_HE = [
-#     "ד מבטל לפעמים את המקף שלפני רביע מוגרש:",
-#     " ח כ;",
-#     " לא לז;",
-#     " לו כא;",
-#     " לז כד.",
-#     " ויש שהוא מבטל גם את המקף שלפני דחי:",
-#     " כח טז;",
-#     " לד לא.",
-#     " והשווה תופעה הפוכה: ט יח."
-# ]
-# _DM_FTNT_16_EN = [
-#     "D [Venice MG (aka RB2)] sometimes omits the מקף before רביע מוגרש:",
-#     " 8:20;",
-#     " 31:37;",
-#     " 36:21;",
-#     " 37:24.",
-#     " And sometimes it also omits the מקף before דחי:",
-#     " 28:16;",
-#     " 34:31.",
-#     " And compare the opposite phenomenon: 9:18."
-# ]
 RECORD_3621 = {
     "qr-noted-by": "nDM",
     "qr-cv": "36:21",

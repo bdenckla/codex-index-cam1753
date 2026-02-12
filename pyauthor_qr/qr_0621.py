7b3917a1f16d38abeb53f2ba451ed3b2c3e260e9
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-12 09:27:14 -0500

    לא and לו

--- pyauthor_qr/qr_0621.py
+++ pyauthor_qr/qr_0621.py
@@ -2,13 +2,13 @@ from pyauthor_util import author
 
 _GENCOM_PARA1 = [
     "Consensus has no כתיב/קרי here, or, if you like,",
-    " לא are the letters of both the כתיב and קרי.",
-    " So, we could say that the consensus כתיב/קרי is לא/לא and",
-    " the proposed כתיב/קרי for μL is לא/לו.",
+    " %לא are the letters of both the כתיב and קרי.",
+    [" So, we could say that the consensus כתיב/קרי is ", author.span_unpointed_tanakh("לא/לא"), " and"],
+    [" the proposed כתיב/קרי for μL is ", author.span_unpointed_tanakh("לא/לו"), "."],
 ]
 _GENCOM_PARA2 = [
     ["Aside: don’t be confused by what might look like an L (ell)"],
-    [" open to the southeast, above the א of לא;"],
+    [" open to the southeast, above the א of %לא;"],
     [" it is the bar of a קמץ connected to a מרכא, both belonging to the ב of"],
     [" ", author.hbo("בָּ֥אוּ"), " on the line above."],
 ]
@@ -17,7 +17,7 @@ RECORD_0621 = {
     "qr-cv": "6:21",
     "qr-consensus": "לֹ֑א",
     "qr-lc-proposed": "ל֑וֹ",
-    "qr-what-is-weird": "קרי of לו not לא",
+    "qr-what-is-weird": "קרי of %לו not %לא",
     "qr-highlight": [1, 2],
     "qr-generic-comment": [author.para(_GENCOM_PARA1), author.para(_GENCOM_PARA2)],
     "qr-lc-loc": {"page": "398B", "column": 2, "line": 20},

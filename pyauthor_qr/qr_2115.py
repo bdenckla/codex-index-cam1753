b2cc3f53c8f215ab464a91246ed61c6a9ea398df
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-17 17:47:20 -0500

    Note that μA lacks מקף in 21:15
    Closes #37

--- pyauthor_qr/qr_2115.py
+++ pyauthor_qr/qr_2115.py
@@ -6,7 +6,10 @@ RECORD_2115 = {
     "qr-consensus": "מַה־שַּׁדַּ֥י",
     "qr-lc-proposed": "מַה־שַׁדַּ֥י",
     "qr-what-is-weird": "ש lacks דגש",
-    "qr-generic-comment": no_dag_after_mah("21:15"),
+    "qr-generic-comment": [
+        no_dag_after_mah("21:15"),
+        " Aside: μA lacks the מקף.",
+    ],
     "qr-highlight": 4,
     "qr-lc-loc": {"page": "403A", "column": 1, "line": 26},
     "qr-ac-loc": {"page": "275v", "column": 1, "line": 20, "word": 1},
